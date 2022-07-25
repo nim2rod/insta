50e40fd99a7b0f0c6998af314c79f25fe94f226d
nimrod aviam <nimrod.a6@gmail.com>
2022-07-25 03:35:33 +0300

    almost comment mode for explore

--- src/views/explore.vue
+++ src/views/explore.vue
@@ -5,24 +5,44 @@
       v-for="explore in explores"
       :key="explore.by.username"
     >
-      <div class="img-container-explore">
-        <img :src="explore.imgUrl" alt="" />
+      <div class="img-container-explore btn">
+        <img @click="viewComments" :src="explore.imgUrl" alt="" />
       </div>
+      <comment
+        v-if="commentMode"
+        @closeComments="closeComments"
+        class="comment-view-container"
+        :story="explore"
+      ></comment>
     </div>
   </section>
 </template>
 
 <script>
+import comment from "../components/comment.mode.cmp.vue";
 import { storyService } from "../services/story.service";
 export default {
   data() {
     return {
       explores: null,
+      commentMode: 0,
     };
   },
   created() {
     this.explores = storyService.createExploreStory();
     console.log("explore", this.explores);
+  },
+  methods: {
+    viewComments(story) {
+      console.log("view comment");
+      this.commentMode = 1;
+    },
+    closeComments() {
+      this.commentMode = 0;
+    },
+  },
+  components: {
+    comment,
   },
 };
 </script>
