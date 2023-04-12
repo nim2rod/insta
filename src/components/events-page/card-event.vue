70063f86e382d32eda2f56eacb3b10488a96b88f
nimrod aviam <nimrod.a6@gmail.com>
2023-04-12 03:16:14 +0300

    finish event page

--- src/components/events-page/card-event.vue
+++ src/components/events-page/card-event.vue
@@ -1,6 +1,13 @@
 <template>
-  <section class="event-card" v-if="event"></section>
-  <h3>{{ event.id }}</h3>
+  <section class="event-card" v-if="event">
+    <h3>{{ event.title }}</h3>
+    <span>{{ toUpper(event.type) }}</span>
+    <div>{{ timeFixed() }}</div>
+    <img :src="event.performers[0].image" alt="" />
+    <div>{{ event.venue.name }}</div>
+    <span>{{ event.venue.address }}</span>
+    <a class="router-link" :href="event.performers[0].url">Order tickets</a>
+  </section>
 </template>
       
       <script>
@@ -12,7 +19,15 @@ export default {
     return {};
   },
   created() {},
-  methods: {},
+  methods: {
+    timeFixed() {
+      let time = this.event.datetime_local;
+      return time.replaceAll("-", " ").replaceAll("T", " At ").slice(0, 19);
+    },
+    toUpper(str) {
+      return str[0].toUpperCase() + str.slice(1, 20);
+    },
+  },
   computed: {},
   components: {},
 };
